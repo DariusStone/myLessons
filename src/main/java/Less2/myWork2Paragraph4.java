21e55edeed94ad02c0aef5f4ff551784285d1420
DariusStone <97939159+DariusStone@users.noreply.github.com>
2022-10-22 11:17:48 +0300

    homeWorkLesson2

---
+++ src/main/java/Less2/myWork2Paragraph4.java
@@ -0,0 +1,13 @@
+package Less2;
+
+public class myWork2Paragraph4 {
+    public static void main(String[] args) {
+        printText(4, "lorem");
+    }
+
+    private static void printText(int times, String str) {
+        for (int j = 0; j < times; j++) {
+            System.out.println(str);
+        }
+    }
+}
